08f83b2fe85804f16314850c21dcba88ad0dadee
Mario Van hessche <mario.vanhessche@gmail.com>
2021-05-31 17:07:50 +0200

    Ended before the api

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-index.js
+  document.addEventListener("mousemove", parallax);
+
+  function parallax(e){
+      this.querySelectorAll('.layer').forEach(layer => {
+          const speed = layer.getAttribute('data-speed')
+  
+          const x = (window.innerWidth - e.pageX*speed)/100
+          const y = (window.innerHeight - e.pageY*speed)/100
+  
+          layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+      })
+  }
+  
+
+  //Buutonsearch
+knop = document.getElementById("btnsearch")
+//when click
+knop.addEventListener("click", function myScript(){
+    var x = document.getElementById("Tekstvak").value;
+    
+    //the fetch code    
+    fetch(`https://restcountries.eu/rest/v2/name/${x}?fullText=true`)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+        citywater = document.getElementById("city")
+        citywater.innerHTML = data[0].city
+        vlagholder = document.getElementById("vlag")
+        vlagholder.src = data[0].flag
+        waterlevel = document.getElementById("habitants")
+        habitants.innerHTML = data[0].population
+        
+       
+        
+    });
+    
+});
